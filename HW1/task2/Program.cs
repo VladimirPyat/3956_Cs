5ed5c58af7426cc4c95b440b3c01fe47f7e663a5
Vladimir Pyat <scared@mail.ru>
2023-02-12 15:23:34 +0300

    task2

--- HW1/task2/Program.cs
+++ HW1/task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 2. Принимаем на вход два числа. Вывод - какое число больше, какое меньше
+
+Console.Write("Number#1: ");
+int number1=Convert.ToInt16(Console.ReadLine());
+Console.Write("Number#2: ");
+int number2=Convert.ToInt16(Console.ReadLine());
+if (number1>number2)
+{
+Console.Write("Maximum: ");
+Console.WriteLine(number1);
+Console.Write("Minimum: ");
+Console.WriteLine(number2);
+}
+else
+{
+Console.Write("Maximum: ");
+Console.WriteLine(number2);
+Console.Write("Minimum: ");
+Console.WriteLine(number1); 
+}
