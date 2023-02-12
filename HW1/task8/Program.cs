85d2b11c127a816298bde717b81bb4c8b1e374c9
Vladimir Pyat <scared@mail.ru>
2023-02-12 20:49:01 +0300

    task 8

---
+++ HW1/task8/Program.cs
@@ -0,0 +1,10 @@
+﻿// задача8. на входе целое число, на выходе все чётные числа от 1 до n
+Console.Write("Введите число: ");
+int number = Convert.ToInt16 (Console.ReadLine());
+for (int i=2;i<=number;i++)
+{
+    if (i%2==0) 
+    {
+        Console.Write(i+" ");
+    }
+}
