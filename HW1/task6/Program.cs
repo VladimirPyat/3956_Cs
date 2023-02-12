18de076b0b4c85a98e073ab1ca764dfce88ff2cc
Vladimir Pyat <scared@mail.ru>
2023-02-12 16:02:48 +0300

    task6

---
+++ HW1/task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// задача 6. на вход число, вывод - четное или нет
+Console.Write("Введите число: ");
+double number = Convert.ToInt16(Console.ReadLine());
+if (number%2==0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("НЕчетное");
+}
