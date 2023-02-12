da7475d228c0cd1079db5eb84b68c4e09622d203
Vladimir Pyat <scared@mail.ru>
2023-02-12 15:48:39 +0300

    task4

---
+++ HW1/task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 4. принять на вход три числа. найти максимум
+Console.WriteLine("Введите 3 числа");
+int number1=Convert.ToInt16(Console.ReadLine());
+int number2=Convert.ToInt16(Console.ReadLine());
+int number3=Convert.ToInt16(Console.ReadLine());
+int max=number1;
+if (number2>max)
+{
+    max=number2;
+}
+if (number3>max)
+{
+    max=number3;
+}
+Console.WriteLine("Максимум равен "+max);
