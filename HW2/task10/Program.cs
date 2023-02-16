900acfdbcf42b5d2cb8cc2e19a0b621835f17966
Vladimir Pyat <scared@mail.ru>
2023-02-16 22:59:01 +0300

    task10

---
+++ HW2/task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int number=0;
+while (number<100||number>999)
+{ 
+    Console.Write("Введите трехзначное число: ");
+    number=Convert.ToInt32(Console.ReadLine());
+}
+int result=(number/10)%10;
+Console.WriteLine("Вторая цифра="+result);
