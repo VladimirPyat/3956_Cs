16556a44481db3cb8b15a3de597d1d47b600bdc9
Vladimir Pyat <scared@mail.ru>
2023-02-19 00:58:53 +0300

    task23

---
+++ HW3/task23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<=n; i++)
+{
+  Console.WriteLine(Math.Pow (i,3));
+
+}
