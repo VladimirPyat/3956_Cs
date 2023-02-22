78473c4a11e45a42f3e5fa26e5f546bdeeb6dda0
Vladimir Pyat <scared@mail.ru>
2023-02-22 23:25:28 +0300

    task25

---
+++ HW4/task25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B - без модуля Math
+Console.Write("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+Console.Write("В какую степень возвести?: ");
+int pow=Convert.ToInt32(Console.ReadLine());
+int result=1;
+for (int i=1; i<=pow; i++)
+{
+  result=result*num;
+}
+Console.Write("Результат возведения в степень: "+result);
