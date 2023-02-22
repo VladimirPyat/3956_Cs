f24b91f9bf733ecddafbb4a51024bb1365140ced
Vladimir Pyat <scared@mail.ru>
2023-02-22 23:49:58 +0300

    task29

---
+++ HW4/task29/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+void ArrayManualFill(int [] array)
+{
+  for (int i=0;i<array.Length;i++)
+    {
+      Console.Write(i+" :");
+      array[i]=Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Console.Write("введите число элементов массива: ");
+int number=Convert.ToInt32(Console.ReadLine());
+int[] myArray=new int [number];
+ArrayManualFill(myArray);
+Console.WriteLine("["+string.Join(", ", myArray)+"]");
