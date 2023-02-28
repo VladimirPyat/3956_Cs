90255288d18775f99928411bf3cc1da2ceee01f3
Vladimir Pyat <scared@mail.ru>
2023-03-01 01:18:31 +0300

    task34,36,38

---
+++ HW5/task38/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+double[] GetRandomArrayD(int length, double leftRange, double rightRange)
+{
+  double[] array = new double[length];
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    int roundAmount = 0;
+    array[i] = Math.Round((new Random().NextDouble()) * (rightRange - leftRange) + leftRange, roundAmount);
+  }
+
+  return array;
+}
+
+(double, double) FindMinMax (double[] array)
+{
+double max=array[0];
+double min=array[0];
+for (int i = 1; i < array.Length; i++)
+{
+  if (array[i]>max) max=array[i];
+  if (array[i]<min) min=array[i];
+}
+return (max, min);
+}
+
+double[] myArrayD = GetRandomArrayD (8, -10, 10);
+Console.Write(string.Join(" ", myArrayD));
+(double maxArray, double minArray) = FindMinMax (myArrayD);
+Console.Write ($" -> {maxArray-minArray}");
