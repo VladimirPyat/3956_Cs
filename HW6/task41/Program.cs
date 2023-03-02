16bb4e1e2ce6f6ff4820ddaa27394f5c1546c8ad
Vladimir Pyat <scared@mail.ru>
2023-03-03 00:26:51 +0300

    task 41

---
+++ HW6/task41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите число или 'stop' для окончания ввода: ");
+string userNumber="";
+int count=0;
+while (true)
+{
+  userNumber=Console.ReadLine();
+  if (userNumber.ToLower()!=("stop"))
+  {
+    if (Convert.ToInt32(userNumber)>0) count++;
+  }  
+  else break;
+} 
+Console.WriteLine("Число чисел больше 0 = "+count);
