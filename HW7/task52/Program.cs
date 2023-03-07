e389e3d13526d5afb9be6fa7632423f23af98ab4
Vladimir Pyat <scared@mail.ru>
2023-03-08 00:27:31 +0300

    task 52

---
+++ HW7/task52/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftBorder = 0, int rightBorder = 10)
+{
+
+    int[,] matrix = new int[rows, columns];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double AverageRow (int row, int[,] matrix)
+{
+  double sum=0;
+  for (int i=0; i<matrix.GetLength(0);i++)
+  {
+    sum+=matrix[i,row];
+  }
+  // Console.WriteLine(sum);
+  double average=sum/matrix.GetLength(0);
+  return average;
+}
+
+int m = ReadNumber("Введите количество строк:");
+int n = ReadNumber("Введите количество столбцов:");
+int[,] myMatrix = GetRandomMatrix(m, n);
+PrintMatrix(myMatrix);
+Console.WriteLine();
+
+for (int j=0; j<myMatrix.GetLength(1); j++)
+{
+  double printAverage = AverageRow (j, myMatrix);
+  Console.WriteLine($"Среднее арифметическое ряда {j} = {printAverage}");
+}
+
+
