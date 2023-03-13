64cf915475c99e8bb788749a9ec428fe01cad906
Vladimir Pyat <scared@mail.ru>
2023-03-13 09:34:58 +0300

    task64

---
+++ HW9/task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int number=8;
+
+void NaturalArray (int n)
+{
+  if (n==0) return ;
+  else 
+  {
+    
+    Console.Write(n+" ");
+    NaturalArray(n-1);
+  }
+}
+
+NaturalArray (number);
